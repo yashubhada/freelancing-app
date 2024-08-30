4c3dd4bae807eebd796c3f4e618feac7e770643a
Yash <cdyash99@gmail.com>
2024-08-30 21:40:56 +0530

    update

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <>
+            <h1 className="text-3xl text-red-600 font-bold underline">
+                Hello world!
+            </h1>
+        </>
+    )
+}
+
+export default App
