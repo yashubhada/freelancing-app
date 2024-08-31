30f4fd00780531defd3ab25c8ced46ebbcdb025a
Yash <cdyash99@gmail.com>
2024-08-31 10:01:26 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from "react-router-dom";
 import Navbar from './components/Navbar'
+import Home from './components/Home';
 
 const App = () => {
     return (
-        <>
+        <BrowserRouter>
             <Navbar />
-        </>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,19 +1,20 @@
 import React from "react";
-import Logo from "../images/logo.png";
+import Logo from "../images/logo-dark.png";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
 
     const toggleNav = () => {
-        document.querySelector(".nav-items").classList.toggle('top-[82px]');
+        document.querySelector(".nav-items").classList.toggle('top-[81px]');
     }
 
     return (
         <>
-            <nav className="bg-white border">
+            <nav className="bg-white">
                 <div className="px-2 py-2 md:px-0 container mx-auto flex items-center justify-between">
                     <div className="flex items-center justify-center">
-                        <img src={Logo} alt="Logo" className="w-16" />
-                        <ul className="absolute right-0 space-y-2 md:space-y-0 top-32 w-full p-2 bg-[#493932] md:bg-white text-white md:text-[#493932] md:p-0 md:static md:flex items-center justify-center md:ml-10 md:space-x-5 nav-items">
+                        <NavLink to='/'><img src={Logo} alt="Logo" draggable={false} className="w-16" /></NavLink>
+                        <ul className="transition-all absolute right-0 space-y-2 -top-32 md:space-y-0 w-full p-2 bg-[#493932] md:bg-white text-white md:text-[#493932] md:p-0 md:static md:flex items-center justify-center md:ml-10 md:space-x-5 nav-items">
                             <li className="cursor-pointer">How We Grow</li>
                             <li className="cursor-pointer">About Us</li>
                             <li className="cursor-pointer">Healthy Living</li>
