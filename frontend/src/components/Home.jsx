432eb9519af2cae53379decb22e77b2113a88295
Yash <cdyash99@gmail.com>
2024-09-02 17:32:11 +0530

    update

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,12 +1,9 @@
 import React from "react";
-import Logo from '../images/logo-light.png';
 
 const Home = () => {
     return(
         <>
-            <section className="bg-[url('./images/hero-img.jpg')] bg-cover w-full h-[40vh] flex items-center justify-center">
-                <img src={Logo} alt="Logo" draggable={false} className="w-32" />
-            </section>
+            
         </>
     );
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,31 +1,37 @@
 import React from "react";
-import Logo from "../images/logo-dark.png";
-import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
 
-    const toggleNav = () => {
-        document.querySelector(".nav-items").classList.toggle('top-[81px]');
+    const toggleMenu = () => {
+        document.querySelector(".menu-items").classList.toggle("hidden");
     }
 
     return (
         <>
-            <nav className="bg-white">
-                <div className="px-2 py-2 md:px-0 container mx-auto flex items-center justify-between">
-                    <div className="flex items-center justify-center">
-                        <NavLink to='/'><img src={Logo} alt="Logo" draggable={false} className="w-16" /></NavLink>
-                        <ul className="transition-all absolute right-0 space-y-2 -top-32 md:space-y-0 w-full p-2 bg-[#493932] md:bg-white text-white md:text-[#493932] md:p-0 md:static md:flex items-center justify-center md:ml-10 md:space-x-5 nav-items">
-                            <li className="cursor-pointer">How We Grow</li>
-                            <li className="cursor-pointer">About Us</li>
-                            <li className="cursor-pointer">Healthy Living</li>
-                        </ul>
+            <nav className="p-3 md:px-0 border-b-[1px] border-b-[#CCC]">
+                <div className="container mx-auto">
+                    <div className="flex items-center justify-between">
+                        <div className="flex items-center justify-center">
+                            <h1 className="text-[#555] mr-2 text-3xl md:hidden" onClick={toggleMenu}><i className="ri-menu-line"></i></h1>
+                            <h1 className="text-xl md:text-2xl font-semibold text-[#14a800] select-none">PROFLEXA</h1>
+                            <ul className="hidden absolute w-full top-[61px] bg-white right-0 space-y-5 p-4 md:static md:flex ml-10 text-sm md:space-x-5 md:space-y-0 md:p-0 menu-items">
+                                <li className="cursor-pointer font-medium hover:text-[#108a00]">Find Freelancers</li>
+                                <li className="cursor-pointer font-medium hover:text-[#108a00]">Find Jobs</li>
+                                <li className="cursor-pointer font-medium hover:text-[#108a00]">About</li>
+                                <li className="md:hidden cursor-pointer font-medium hover:text-[#108a00]">Sign up</li>
+                                <li className="md:hidden cursor-pointer font-medium hover:text-[#108a00]">Log in</li>
+                            </ul>
+                        </div>
+                        <div className="flex items-center">
+                            <button className="font-medium text-sm text-white bg-[#108a00] hover:bg-[#14a800] py-2 px-3 rounded">
+                                Post a Job
+                            </button>
+                            <ul className="hidden md:flex ml-10 text-sm space-x-4">
+                                <li className="cursor-pointer font-medium hover:text-[#108a00]">Sign up</li>
+                                <li className="cursor-pointer font-medium hover:text-[#108a00]">Log in</li>
+                            </ul>
+                        </div>
                     </div>
-                    <ul className="flex items-center justify-center space-x-5">
-                        <li className="flex items-center cursor-pointer"><i className="ri-search-line text-[#493932] text-xl"></i><span className="hidden md:block ml-1 text-sm">Search</span></li>
-                        <li className="flex items-center cursor-pointer"><i className="ri-user-line text-[#493932] text-xl"></i><span className="hidden md:block ml-1 text-sm">Login</span></li>
-                        <li className="flex items-center cursor-pointer"><i className="ri-shopping-cart-line text-[#493932] text-xl"></i><span className="hidden md:block ml-1 text-sm">Cart</span></li>
-                        <li className="md:hidden flex items-center cursor-pointer" onClick={toggleNav}><i className="ri-menu-3-line text-[#493932] text-xl"></i></li>
-                    </ul>
                 </div>
             </nav>
         </>
