0a2a8c1460a7b3d4f122a82b44fda727a7cff1c0
Yash <cdyash99@gmail.com>
2024-10-10 18:45:04 +0530

    update

--- frontend/src/components/employer-dashboard/EmployeApplicants.jsx
+++ frontend/src/components/employer-dashboard/EmployeApplicants.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import NavbarEmp from './NavbarEmp';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
@@ -101,10 +101,26 @@ const EmployeApplicants = () => {
         { id: 1, text: 'Hello! How are you?', sender: 'me' },
         { id: 2, text: 'I’m good, thank you! How about you?', sender: 'other' },
         { id: 3, text: 'I’m great, thanks for asking!', sender: 'me' },
+        { id: 4, text: 'I’m great, thanks for asking! I’m great, thanks for asking! I’m great, thanks for asking!', sender: 'me' },
         { id: 5, text: 'I’m great, thanks for asking! I’m great, thanks for asking! I’m great, thanks for asking!', sender: 'me' },
-        { id: 6, text: 'Good to hear!', sender: 'other' },
+        { id: 6, text: 'I’m great, thanks for asking! I’m great, thanks for asking! I’m great, thanks for asking!', sender: 'me' },
+        { id: 7, text: 'I’m great, thanks for asking! I’m great, thanks for asking! I’m great, thanks for asking!', sender: 'me' },
+        { id: 8, text: 'I’m great, thanks for asking! I’m great, thanks for asking! I’m great, thanks for asking!', sender: 'me' },
+        { id: 9, text: 'Good to hear!', sender: 'other' },
     ];
     const [showMessages, setShowMessages] = useState(false);
+    const [isOpenConversation, setIsOpenConversation] = useState(false);
+    const chatContainerRef = useRef(null);
+
+    const scrollToBottom = () => {
+        if (chatContainerRef.current) {
+            chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+        }
+    };
+
+    useEffect(() => {
+        scrollToBottom();
+    }, [isOpenConversation]);
 
     return (
         <>
@@ -251,7 +267,7 @@ const EmployeApplicants = () => {
                     </div>
                     {/* conversation */}
                     <div className='mt-[50px]'>
-                        <div className='flex items-center p-2 cursor-pointer hover:bg-[#f7f7f7] border-b'>
+                        <div className='flex items-center p-2 cursor-pointer hover:bg-[#f7f7f7] border-b' onClick={() => setIsOpenConversation(true)}>
                             <div className='w-10 h-10 mr-2'>
                                 <img className='w-full h-full object-cover rounded-full' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLQUQ6g6NjGqj3qncgsJGpxzzRrL_qDAc1qQ&s" alt="Profile image" />
                             </div>
@@ -299,51 +315,54 @@ const EmployeApplicants = () => {
                     </div>
                 </section>
             }
-            <section className="absolute right-1 bottom-0 bg-[#fcfcfc] flex flex-col w-full md:w-[500px] rounded-t-md">
-                <div className='flex justify-between items-center p-2 border-b mb-2'>
-                    <div className='hidden md:flex items-center'>
-                        <div className='w-8 h-8 relative'>
-                            <img className='w-full h-full object-cover rounded-full' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLQUQ6g6NjGqj3qncgsJGpxzzRrL_qDAc1qQ&s" alt="Profile image" />
-                            <div className='absolute right-0 bottom-0'>
-                                <div className='h-3 w-3 bg-green-500 border border-white rounded-full'></div>
+            {
+                isOpenConversation &&
+                <section className="z-20 absolute right-0 md:right-1 h-screen md:h-fit bottom-0 bg-[#fcfcfc] flex flex-col w-full md:w-[500px] md:rounded-t-md md:border">
+                    <div className='flex justify-between items-center p-2 border-b mb-2'>
+                        <div className='flex items-center'>
+                            <div className='w-8 h-8 relative'>
+                                <img className='w-full h-full object-cover rounded-full' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLQUQ6g6NjGqj3qncgsJGpxzzRrL_qDAc1qQ&s" alt="Profile image" />
+                                <div className='absolute right-0 bottom-0'>
+                                    <div className='h-3 w-3 bg-green-500 border border-white rounded-full'></div>
+                                </div>
                             </div>
+                            <p className='text-sm font-medium ml-2'>Yash patel</p>
                         </div>
-                        <p className='text-sm font-medium ml-2'>Messaging</p>
-                    </div>
-                    <button onClick={() => setShowMessages(true)} className='text-base font-medium text-white bg-indigo-600 px-2 py-1 rounded-full'>
-                        <i className="ri-close-fill"></i>
-                    </button>
-                </div>
-                <div className="max-h-[400px] overflow-y-auto hide-scrollbar space-y-1 px-2">
-                    {messages.map((message) => (
-                        <div
-                            key={message.id}
-                            className={`flex ${message.sender === 'me' ? 'justify-end' : 'justify-start'}`}
-                        >
-                            <div
-                                className={`max-w-64 p-2 rounded-lg text-white text-sm ${message.sender === 'me' ? 'bg-indigo-500' : 'bg-gray-500'
-                                    }`}
-                            >
-                                {message.text}
-                            </div>
-                        </div>
-                    ))}
-                </div>
-
-                {/* Input Area */}
-                <div className='p-2'>
-                    <div className='bg-white rounded-md p-2 flex items-center justify-between w-full border mt-4 focus-within:border-indigo-500'>
-                        <input
-                            type="text"
-                            placeholder="Type a message"
-                            className="bg-transparent outline-none bottom-0 w-full pr-2"
-                        />
-                        <button className="bg-[#14a800] text-white rounded-full w-8 h-8 flex items-center justify-center text-base">
-                            <i className="ri-send-plane-2-fill"></i>
+                        <button onClick={() => setIsOpenConversation(false)} className='text-base font-medium text-white bg-indigo-600 px-2 py-1 rounded-full'>
+                            <i className="ri-close-fill"></i>
                         </button>
                     </div>
-                </div>
-            </section>
+                    <div ref={chatContainerRef} className="mb-[66px] md:max-h-[400px] overflow-y-auto hide-scrollbar space-y-1 px-2">
+                        {messages.map((message) => (
+                            <div
+                                key={message.id}
+                                className={`flex ${message.sender === 'me' ? 'justify-end' : 'justify-start'}`}
+                            >
+                                <div
+                                    className={`max-w-64 p-2 rounded-lg text-white text-sm ${message.sender === 'me' ? 'bg-indigo-500' : 'bg-gray-500'
+                                        }`}
+                                >
+                                    {message.text}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+
+                    {/* Input Area */}
+                    <div className='p-2 fixed w-full bottom-0 z-10 bg-[#fcfcfc]'>
+                        <div className='bg-white rounded-md p-2 flex items-center justify-between w-full border focus-within:border-indigo-500'>
+                            <input
+                                type="text"
+                                placeholder="Type a message"
+                                className="bg-transparent outline-none bottom-0 w-full pr-2"
+                            />
+                            <button className="bg-[#14a800] text-white rounded-full w-8 h-8 flex items-center justify-center text-base">
+                                <i className="ri-send-plane-2-fill"></i>
+                            </button>
+                        </div>
+                    </div>
+                </section>
+            }
         </>
     );
 };
